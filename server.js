804927660323fbef3e00ee088d03d748b99654e2
Khaled Elgendy <elgendy1204@gmail.com>
2021-11-13 22:58:12 +0100

    Create basic grpc service and client using to small microservices

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const greet = require("./protos/greet_pb");
+
+const reply = new greet.HelloReply();
+
+const PROTO_PATH = __dirname + "/./protos/greet.proto";
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
 function sayHello(call, callback) {
-  callback(null, { message: "Hello " + call.request.name });
+  reply.message = "Hello " + call.request.name;
+  callback(null, reply);
 }
 
 function sayHelloAgain(call, callback) {
-  callback(null, { message: "Hello again, " + call.request.name });
+  reply.message = "Hello again, " + call.request.name;
+  callback(null, reply);
 }
 
-function main() {
-  var server = new grpc.Server();
-
-  server.addService(hello_proto.Greeter.service, {
-    sayHello: sayHello,
-    sayHelloAgain: sayHelloAgain,
-  });
-
-  server.bindAsync(
-    "0.0.0.0:50051",
-    grpc.ServerCredentials.createInsecure(),
-    () => {
-      server.start();
-    }
-  );
-}
+const server = new grpc.Server();
+server.addService(protoDescriptor.Greeter.service, {
+  sayHello,
+  sayHelloAgain,
+});
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    server.start();
+  }
+);
